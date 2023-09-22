aec3466600df813e8f2b56e5c53ca2057d3c8061
mcmcCat <526008246@qq.com>
2023-09-22 17:28:12 +0800

    test

--- publish.sh
+++ publish.sh
@@ -6,17 +6,3 @@ set -e
 # 进入packages中进行打包发布
 cd packages
 
-# 读取package.json中的version
-version=`jq -r .version package.json`
-
-pnpm build
-
-npm publish--registry=https://registry.npmjs.org/
-
-# 升级 vue-amazing-ui 依赖版本
-pnpm up mmcat-ui@$version
-
-# 提交版本更新代码到github
-git add .
-git cm -m "update $version"
-git push
